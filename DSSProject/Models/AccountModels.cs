0074d3f82cc5a5eaf4c826c6a77ebd93987b2f59
ArsiTech <haydarmehryar@yahoo.com>
2015-02-04 17:58:32 +0330

    web.release.config db transform

--- DSSProject/Models/AccountModels.cs
+++ DSSProject/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace DSSProject.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("name=DefaultConnection")
         {
         }
 
